6cbe8b8a3cea9b13eadcbe725f423f10f35b2455
Azure-Agst <me@azureagst.dev>
2023-05-12 01:26:03 -0400

    Update sidebar logic again to allow nonstandard configs

--- src/sidebar/Sidebar.tsx
+++ src/sidebar/Sidebar.tsx
@@ -14,31 +14,23 @@ export const Sidebar: VFC = ({}) => {
 
     useEffect(() => {
 
-        // Get service status from backend
-        Backend.getStStatus().then(result => {
-
-            // If server is live...
-            if (result == 0) {
-
-                // Send a ping
-                // If we get a pong, 0 is correct!
-                // If we error out, bad config!
-                syncThingFetch<iStPing>("/rest/system/ping").then(() => {
-                    setStStatus(0)
-                }).catch(() => {
-                    setStStatus(5)
-                })
-            } else {
-                setStStatus(result)
-            }
-                
+        // Send a ping
+        // If we get a pong, 0 is correct!
+        // If we error out, something is wrong!
+        // - Get service status from backend
+        syncThingFetch<iStPing>("/rest/system/ping").then(() => {
+            setStStatus(5)
+        }).catch(() => {
+            Backend.getStStatus().then(result => {
+                setStStatus(result)     
+            })
         })
- 
+
     }, [])
 
-    // If loaded successfully...
-    if (stStatus == 0) 
-        return (<SbMainView />)
+    // If config is invalid...
+    if (stStatus == 0)
+        return (<SbBadConfigView />)
 
     // If service not loaded...
     if (stStatus == 3)
@@ -48,9 +40,9 @@ export const Sidebar: VFC = ({}) => {
     if (stStatus == 4)
         return (<SbNotFoundView />)
     
-    // If config is invalid...
-    if (stStatus == 5)
-        return (<SbBadConfigView />)
+    // If loaded successfully...
+    if (stStatus == 5) 
+        return (<SbMainView />)
 
     // Default statement
     return (
