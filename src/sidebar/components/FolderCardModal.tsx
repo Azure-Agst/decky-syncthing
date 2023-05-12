6b3b9f09b8d338cc620b63eefcfb6f014e4261ee
Azure-Agst <me@azureagst.dev>
2023-05-12 13:41:30 -0400

    Beautify folder modal

--- src/sidebar/components/FolderCardModal.tsx
+++ src/sidebar/components/FolderCardModal.tsx
@@ -2,7 +2,18 @@ import { VFC } from "react"
 import { ConfirmModal } from "decky-frontend-lib"
 
 import { iFolderStatus } from "../../types"
-import { noMargin } from "./FolderCard.css"
+import { statusColor } from "./FolderCard.css"
+import { fullStatus } from "../../utils/Folder"
+
+export const humanFileSize = (bytes: number): string => {
+    let units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB']
+    let thresh = 1000; let i = 0;
+    while (Math.abs(bytes) >= thresh) {
+        bytes /= thresh;
+        i++;
+    }
+    return bytes.toFixed(2) + ' ' + units[i];
+}
 
 export const FolderCardModal: VFC<{data: iFolderStatus, closeModal?(): void}> = ({ data, closeModal }) => {
     return (
@@ -10,12 +21,20 @@ export const FolderCardModal: VFC<{data: iFolderStatus, closeModal?(): void}> = 
             closeModal={closeModal}
             bAlertDialog={true}
             strOKButtonText={"Dismiss"}
-            strTitle={data.folder.label}
-            >
-            <p style={noMargin}>Status: {data.dbStatus.state}</p>
-            { data.stats && 
-                <p style={noMargin}>Last File: {data.stats.lastFile.filename}</p>
+            strTitle={<span>
+                {data.folder.label} - <span style={statusColor(data)}>{fullStatus(data)}</span>
+            </span>}>
+
+            <div><b>Folder ID:</b> {data.folder.id}</div>
+            <div><b>Folder Path:</b> {data.folder.path}</div>
+            <div>{data.dbStatus.globalFiles} files, {data.dbStatus.globalDirectories} directories, ~{humanFileSize(data.dbStatus.globalBytes)}</div>
+
+            { data.stats && <br/> }
+            { data.stats && <div><b>Last Scan:</b> {data.stats.lastScan.toLocaleString()}</div> }
+            { data.stats && data.stats.lastFile.filename != "" &&
+                <div><b>Last File:</b> {data.stats.lastFile.filename} @ {data.stats.lastFile.at.toLocaleString()}</div>
             }
+
         </ConfirmModal>
     )
 }
